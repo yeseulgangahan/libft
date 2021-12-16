5c55f465029030842f14af0706b6f76be86c5c2e
yeseul <letmeshare46@gmail.com>
2021-12-16 15:54:57 +0900

    하나만 널일 때 NULL 대신 반환되도록 처리함

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -4,8 +4,12 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*s3;
 
-	if (s1 == NULL || s2 == NULL)
+	if (s1 == NULL && s2 == NULL)
 		return (NULL);
+	if (s1 == NULL)
+		return (ft_strdup(s2));
+	if (s2 == NULL)
+		return (ft_strdup(s1));
 	s3 = (char *)ft_calloc((ft_strlen(s1) + ft_strlen(s2) + 1), sizeof(char));
 	if (s3 == NULL)
 		return (NULL);
