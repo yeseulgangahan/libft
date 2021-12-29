71c74911dc8351e0168f948680e32568a046b669
yeseul <letmeshare46@gmail.com>
2021-12-29 14:43:17 +0900

    처음 작성

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -1,1 +1,27 @@
+#include "libft_bonus.h"
 
+t_list	*ft_lstmap(t_list *lst, void (*f)(void *), void (*del)(void *))
+{
+	t_list	*new;
+	t_list	*tmp;
+
+	if (lst == NULL || f == NULL || del == NULL)
+		return ;
+	new = ft_lstnew(lst->content);
+	if (new == NULL)
+		return (NULL);
+	tmp = new;
+	lst = lst->next;
+	while (lst)
+	{
+		tmp->next = ft_lstnew(lst->content);
+		if (tmp->next == NULL)
+		{
+			ft_lstclear(&new, del);
+			return (NULL);
+		}
+		tmp = tmp->next;
+		lst = lst->next;
+	}
+	return (new);
+}
