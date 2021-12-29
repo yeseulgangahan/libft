ca1f2706d14730ffe32cf8a9ad57ca455bac9235
yeseul <letmeshare46@gmail.com>
2021-12-29 13:43:38 +0900

    인자 널일때 리턴하도록 함, 해제하는 부분 ft_lstdelone함수 사용으로 대체

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -4,11 +4,12 @@ void	ft_lstclear(t_list **lst, void (*del)(void *))
 {
 	t_list	*tmp;
 
+	if (lst == NULL || del == NULL)
+		return ;
 	while (*lst)
 	{
 		tmp = (*lst)->next;
-		del((*lst)->content);
-		free(*lst);
+		ft_lstdelone(*lst, del);
 		*lst = tmp;
 	}
 }
