5372d1c2190dd6a5203fae4c20d328affadc096e
yeseul <letmeshare46@gmail.com>
2021-12-29 15:07:37 +0900

    42header

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -1,26 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstmap_bonus.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yehan <yehan@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/12/29 15:05:54 by yehan             #+#    #+#             */
+/*   Updated: 2021/12/29 15:06:04 by yehan            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft_bonus.h"
 
 t_list	*ft_lstmap(t_list *lst, void (*f)(void *), void (*del)(void *))
 {
 	t_list	*new;
-	t_list	*tmp;
+	int		size;
 
 	if (lst == NULL || f == NULL || del == NULL)
 		return ;
 	new = ft_lstnew(lst->content);
 	if (new == NULL)
 		return (NULL);
-	tmp = new;
 	lst = lst->next;
 	while (lst)
 	{
-		tmp->next = ft_lstnew(lst->content);
-		if (tmp->next == NULL)
+		size = ft_lstsize(new);
+		ft_lstadd_back(&new, ft_lstnew(lst->content));
+		if (size == ft_lstsize(new))
 		{
 			ft_lstclear(&new, del);
 			return (NULL);
 		}
-		tmp = tmp->next;
 		lst = lst->next;
 	}
 	return (new);
