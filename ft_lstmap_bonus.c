b2f1cda1d95ff4ff79e5408a533a9daba7ccf25d
yeseul <letmeshare46@gmail.com>
2021-12-29 19:43:15 +0900

    대폭 수정

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,33 +6,32 @@
 /*   By: yehan <yehan@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/29 15:05:54 by yehan             #+#    #+#             */
-/*   Updated: 2021/12/29 15:46:58 by yehan            ###   ########.fr       */
+/*   Updated: 2021/12/29 19:36:01 by yehan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-
-t_list	*ft_lstmap(t_list *lst, void (*f)(void *), void (*del)(void *))
+t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 {
-	t_list	*new;
+	t_list	*lst2;
 	int		size;
 
 	if (lst == NULL || f == NULL || del == NULL)
 		return (NULL);
-	new = ft_lstnew(lst->content);
-	if (new == NULL)
+	lst2 = ft_lstnew(f(lst->content));
+	if (lst2 == NULL)
 		return (NULL);
 	lst = lst->next;
 	while (lst)
 	{
-		size = ft_lstsize(new);
-		ft_lstadd_back(&new, ft_lstnew(lst->content));
-		if (size == ft_lstsize(new))
+		size = ft_lstsize(lst2);
+		ft_lstadd_back(&lst2, ft_lstnew(f(lst->content)));
+		if (size == ft_lstsize(lst2))
 		{
-			ft_lstclear(&new, del);
+			ft_lstclear(&lst2, del);
 			return (NULL);
 		}
 		lst = lst->next;
 	}
-	return (new);
+	return (lst2);
 }
