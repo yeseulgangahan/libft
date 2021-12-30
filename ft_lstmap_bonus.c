87ae933234991b06f8f491d04e6cacc5fd2d2ed0
yeseul <letmeshare46@gmail.com>
2021-12-30 13:16:07 +0900

    3번째 인자 사용이 필요 없을 때는 NULL포인터가 들어올 수 있도록 가드하지 않는다.

--- ft_lstmap_bonus.c
+++ ft_lstmap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: yehan <yehan@student.42seoul.kr>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/29 15:05:54 by yehan             #+#    #+#             */
-/*   Updated: 2021/12/29 20:13:32 by yehan            ###   ########.fr       */
+/*   Updated: 2021/12/30 13:04:40 by yehan            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ t_list	*ft_lstmap(t_list *lst, void *(*f)(void *), void (*del)(void *))
 	t_list	*lst2;
 	t_list	*tmp;
 
-	if (lst == NULL || f == NULL || del == NULL)
+	if (lst == NULL || f == NULL)
 		return (NULL);
 	lst2 = ft_lstnew(f(lst->content));
 	if (lst2 == NULL)
