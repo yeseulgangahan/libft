4af892d8606ceb48498170206235aacd07cb3678
Yeseul Han <yehan@c5r2s1.42seoul.kr>
2022-06-03 08:16:02 +0900

    gnl 폴더의 최종커밋이 놈을 안 지켜서.. 제출했던 코드를 가져옴

--- include/get_next_line.h
+++ include/get_next_line.h
@@ -1,16 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yehan <yehan@student.42seoul.kr>           +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/03 07:53:10 by yehan             #+#    #+#             */
+/*   Updated: 2022/06/03 08:14:04 by yehan            ###   ########seoul.kr  */
+/*                                                                            */
+/* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
 # include <unistd.h>
 # include <stdlib.h>
-# include "../include/libft.h"
+# include <stdbool.h>
 
-# define BUFFER_SIZE 10000
+# define BUFFER_SIZE 1000
 
 char	*get_next_line(int fd);
-char	*read_iter(char **s_save, int fd);
+char	*read_iter(char *s_save, int fd);
 char	*get_line(char const *s_save);
-char	*set_remains(char **s_save, size_t offset);
-
+bool	set_remains(char **s_save, size_t offset);
 #endif
